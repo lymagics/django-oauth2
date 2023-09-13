ad4c730f6b60121f73cb7e935d19657e7e0109ea
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-13 11:31:35 +0300

    feat(users): add endpoint to get user by id.

--- authy/users/api/views.py
+++ authy/users/api/views.py
@@ -1,9 +1,11 @@
+from django.http import Http404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from core.decorators import input
-from users import services
-from users.api.schemas import UserIn
+from core.decorators import input, output
+from users import services, selectors
+from users.api.schemas import UserIn, UserOut
 
 
 @api_view(['POST'])
@@ -16,3 +18,12 @@ def user_create(request):
         data['password'],
     )
     return Response(status=200)
+
+
+@api_view(['GET'])
+@output(UserOut)
+def user_get(request, pk: int):
+    user = selectors.user_get(pk)
+    if user is None:
+        raise Http404
+    return user
--- authy/users/urls.py
+++ authy/users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from users.api import views
 
 urlpatterns = [
+    path('<int:pk>/', views.user_get, name='get'),
     path('create/', views.user_create, name='create'),
 ]
 
