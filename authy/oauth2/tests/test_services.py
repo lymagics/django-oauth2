817cb515d9444b9f53fd42277203cf4c9635d333
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-13 13:44:53 +0300

    test: add test for oauth2 callback.

--- authy/oauth2/tests/test_services.py
+++ authy/oauth2/tests/test_services.py
@@ -1,11 +1,14 @@
-from django.test import SimpleTestCase
+from unittest.mock import patch, Mock
+
+from django.test import TestCase
 
 from oauth2 import services
 from oauth2.errors import OAuth2Error
 from oauth2.models import OAuth2
+from users.tests.factories import UserFactory
 
 
-class TestServices(SimpleTestCase):
+class TestServices(TestCase):
     """
     Test case for oauth2 services.
     """
@@ -22,3 +25,19 @@ class TestServices(SimpleTestCase):
     def test_oauth2_authorize_fail(self):
         with self.assertRaises(OAuth2Error):
             services.oauth2_authorize('unsupported')
+
+    @patch('oauth2.services.queries.get_access_token')
+    @patch('oauth2.services.queries.get_user_info')
+    @patch('oauth2.services.user_get_by_email')
+    def test_oauth2_callback(
+        self,
+        user_get_by_email_mock: Mock,
+        get_user_info_mock: Mock,
+        get_access_token_mock: Mock,
+    ):
+        new_user = UserFactory()
+        user_get_by_email_mock.return_value = new_user
+        get_user_info_mock.return_value = '123@example.com'
+        get_access_token_mock.return_value = '123'
+        user = services.oauth2_callback('google', '123', 'abc', 'abc')
+        self.assertEqual(user, new_user)
