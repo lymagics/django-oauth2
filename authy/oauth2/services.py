04161558a01b2ead0f1e9224ee9211d270d39001
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-13 13:36:14 +0300

    feat(oauth2): add oauth2 callback service.

--- authy/oauth2/services.py
+++ authy/oauth2/services.py
@@ -3,8 +3,12 @@ from urllib.parse import urlencode
 
 from django.conf import settings
 
+from oauth2 import queries
 from oauth2.errors import OAuth2Error
 from oauth2.models import OAuth2
+from users.models import User
+from users.selectors import user_get_by_email
+from users.services import user_create
 
 
 def oauth2_authorize(provider_name: str) -> OAuth2:
@@ -33,5 +37,35 @@ def oauth2_callback(
     code: str,
     state: str,
     session_state: str
-):
-    pass
+) -> User:
+    provider = settings.OAUTH2_PROVIDERS.get(provider_name)
+    if provider is None:
+        error = 'Unsupported provider.'
+        raise OAuth2Error(error)
+    if state != session_state:
+        error = 'OAuth2 state is incorrect.'
+        raise OAuth2Error(error)
+    if code is None:
+        error = 'OAuth2 code was empty.'
+        raise OAuth2Error(error)
+    
+    oauth2_token = queries.get_access_token(
+        provider, provider_name, code
+    )
+    if oauth2_token is None:
+        error = 'Failed to get access token.'
+        raise OAuth2Error(error)
+    
+    email = queries.get_user_info(provider, oauth2_token)
+    if email is None:
+        error = 'Failed to get user info.'
+        raise OAuth2Error(error)
+    
+    user = user_get_by_email(email)
+    if user is not None:
+        return user
+    return user_create(
+        email=email,
+        username=email.split('@')[0],
+        password=secrets.token_urlsafe(16),
+    )
