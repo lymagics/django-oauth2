b35c5d7c9f9d26c775943142a25a724f97f11059
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-13 13:30:01 +0300

    feat(users): add user get by email selector.

--- authy/users/selectors.py
+++ authy/users/selectors.py
@@ -3,3 +3,7 @@ from users.models import User
 
 def user_get(user_id: int) -> User:
     return User.objects.filter(pk=user_id).first()
+
+
+def user_get_by_email(email: str) -> User:
+    return User.objects.filter(email=email).first()
